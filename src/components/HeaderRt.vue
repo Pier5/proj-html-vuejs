a81d42d3af9d95b98415a6ec80ca037ecab4e87f
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-04-01 17:11:00 +0200

    -header work in progress-

--- src/components/HeaderRt.vue
+++ src/components/HeaderRt.vue
@@ -1,12 +1,53 @@
 <template>
   <header>
-
+    <div class="container">
+      <img class="head-logo" src="../assets/img/light-logo.png" alt="logo azienda MaxCoach">
+      <nav>
+        <ul>
+          <li class="list-item" v-for="item in arrNavBar" :key="item">
+            <a class="link-item" href="item.link">{{ item.text }}</a>
+            <i class="fa-solid fa-angle-down"></i>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
 
 <script>
 export default {
-  name: 'HeaderRt'
+  name: 'HeaderRt',
+  data () {
+    return {
+      arrNavBar: [
+        {
+          text: 'Home',
+          link: '#'
+        },
+        {
+          text: 'Pages',
+          link: '#'
+        },
+        {
+          text: 'Courses',
+          link: '#'
+        },
+        {
+          text: 'Features',
+          link: '#'
+        },
+        {
+          text: 'Blog',
+          link: '#'
+        },
+        {
+          text: 'Shop',
+          link: '#'
+        }
+
+      ]
+    }
+  }
 }
 </script>
 
@@ -17,5 +58,27 @@ header {
   width: 100%;
   height: 5rem;
   background-color: $rhino-color;
+  display: flex;
+  align-items: center;
+  .head-logo {
+    width: 10%;
+    margin-right: auto;
+  }
+  .list-item {
+    display: inline-block;
+    margin-right: 40px;
+     .link-item {
+        color: $white-color;
+        text-decoration: none;
+        font-size: .9rem;
+        font-weight: bolder;
+      }
+      .fa-angle-down {
+        color: $white-color;
+        font-size: .6rem;
+        margin-left: 5px;
+      }
+  }
+
 }
 </style>
