28b1280ca06d4c4f12ced83e6a8fd3da28e86d57
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-04-01 15:55:47 +0200

    -primo commit-

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderRt />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderRt from './components/HeaderRt'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderRt
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/styles/style.scss';
 </style>
---
+++ src/components/HeaderRt.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderRt'
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/styles/partials/variables.scss";
+
+header {
+  width: 100%;
+  height: 5rem;
+  background-color: $rhino-color;
+}
+</style>
