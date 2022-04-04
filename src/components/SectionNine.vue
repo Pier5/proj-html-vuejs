c386445f60e0472ca87def5f146a319e77645b17
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-04-04 17:45:04 +0200

    -pallini sezione nove-

--- src/components/SectionNine.vue
+++ src/components/SectionNine.vue
@@ -1,5 +1,8 @@
 <template>
   <section class="section-nine">
+    <div class="circle-one"></div>
+    <div class="circle-two"></div>
+    <div class="circle-three"></div>
     <h2>Start today for getting <span class="green-text">Online Certification</span></h2>
     <h1>You can be your own guiding star with our help!</h1>
     <button class="main-btn">Get started now</button>
@@ -17,6 +20,34 @@ export default {
 .section-nine {
   text-align: center;
   padding: 50px 0 80px;
+  position: relative;
+  .circle-one {
+    position: absolute;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: $anzac-color;
+    left: 80px;
+    bottom: 120px;
+  }
+  .circle-two {
+    position: absolute;
+    width: 35px;
+    height: 35px;
+    border-radius: 50%;
+    background-color: $mandy-color;
+    right: 370px;
+    bottom: 235px;
+  }
+  .circle-three {
+    position: absolute;
+    width: 33px;
+    height: 33px;
+    border-radius: 50%;
+    background-color: #aab8f1;
+    right: 170px;
+    bottom: 120px;
+  }
   h2 {
     font-size: 1rem;
     .green-text {
